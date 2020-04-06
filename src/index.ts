2f9cba5666b8dc41161ebe4986b14f6c50066da3
hank <steven-online@hotmail.com>
2020-04-07 00:27:53 +0800

    fix: update entry file

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,7 @@
 import "./components/wx-button";
+import "./components/wx-group";
+import "./components/wx-icon";
+import "./components/wx-switch";
+import WxMessage from "./components/wx-message";
+
+window["WxMessage"] = WxMessage;
